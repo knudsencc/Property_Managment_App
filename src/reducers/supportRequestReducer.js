b3cd604d78664d27aa7e98632b689a3437ccaf07
Cameron KNUDSEN <tmphotock@gmail.com>
2018-05-09 15:01:42 -0600

    added support reducer

---
+++ src/reducers/supportRequestReducer.js
@@ -0,0 +1,40 @@
+import { 
+  SELECT_REQUEST_TYPE, 
+  FETCH_SUPPORT_REQUESTS,
+  FETCH_SUPPORT_REQUEST_BY_ID
+} from "../actions/types";
+
+const INITIAL_STATE = {
+  selected: 'pending',
+  supportRequests: [],
+  fetchedItem: []
+}
+ 
+export default function(state = INITIAL_STATE, action) {
+  switch(action.type) {
+    case SELECT_REQUEST_TYPE:
+      const selected = action.payload
+      return {
+        ...state,
+        selected
+      }
+    case FETCH_SUPPORT_REQUESTS:
+      const supportRequests = action.payload;
+
+      return {
+        ...state,
+        supportRequests
+      }
+    case FETCH_SUPPORT_REQUEST_BY_ID:
+      const fetchedItem = state.supportRequests.find(supportRequest => {
+        if (supportRequest._id == action.payload) {
+          return supportRequest
+        }
+      })
+      return {
+          ...state,
+          fetchedItem
+        }
+    default: return state;
+  }
+}
