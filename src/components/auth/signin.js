dc681fc0323b875e6b2186850ebd49cd872db054
Cameron KNUDSEN <tmphotock@gmail.com>
2018-05-09 14:48:47 -0600

    added sign in and sign up

---
+++ src/components/auth/signin.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+import { connect } from 'react-redux';
+
+class Signin extends Component {
+
+  renderInput(field) {
+    return <input className="form-control" {...field.input} type="field.type"/>
+  }
+
+  handleFormSubmit({email, password}) {
+    this.props.signinUser({email, password})
+  }
+ 
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <label htmlFor="email">Email</label>
+        <Field name="email" component={this.renderInput} type="text"/>
+        <label htmlFor="password">Password</label>
+        <Field name="password" component={this.renderInput} type="password"/>
+
+        <button action="submit" className="btn btn-primary">Sign In</button>
+      </form>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return { state }
+}
+
+Signin = reduxForm({form: "signin"})(Signin);
+export default connect(mapStateToProps, actions)(Signin);
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class Signup extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        sign up form
       </div>
-    );
+    )
   }
 }
+ 
+export default Signup;
