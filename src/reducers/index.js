848404c2f1c64ac6b171db7d750234e93f99db7f
Cameron KNUDSEN <tmphotock@gmail.com>
2018-05-09 14:58:32 -0600

    updated reducer index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as form} from 'redux-form';
+import newsletterReducer from './newsletterReducer';
+import supportRequestReducer from './supportRequestReducer';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  form,
+  newsletter: newsletterReducer,
+  supportRequest: supportRequestReducer
 });
 
 export default rootReducer;
